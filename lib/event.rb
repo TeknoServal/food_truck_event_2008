6f97c59fcb15a9836ff10472a31036cd94e462bc
Aidan Murray <aidanhansm@gmail.com>
2020-09-15 13:59:04 -0600

    Add method food trucks that sell

--- lib/event.rb
+++ lib/event.rb
@@ -16,4 +16,8 @@ class Event
   def food_truck_names
     @food_trucks.map(&:name)
   end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.select { |truck| truck.inventory.keys.include?(item) }
+  end
 end
