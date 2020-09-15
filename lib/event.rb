cab88ff4a9246bb1b056a8f92f189165bc91e3fa
Aidan Murray <aidanhansm@gmail.com>
2020-09-15 14:35:52 -0600

    Add method total inventory

--- lib/event.rb
+++ lib/event.rb
@@ -20,4 +20,24 @@ class Event
   def food_trucks_that_sell(item)
     @food_trucks.select { |truck| truck.inventory.keys.include?(item) }
   end
+
+  def items
+    inventories = @food_trucks.map(&:inventory)
+
+    inventories.each_with_object({}) do |inventory, output|
+      inventory.each do |item, amount|
+        if output[item]
+          output[item] += amount
+        else
+          output[item] = amount
+        end
+      end
+    end
+  end
+
+  def total_inventory
+    items.each_with_object({}) do |item, output|
+      output[item[0]] = { quantity: item[1], food_trucks: food_trucks_that_sell(item[0]) }
+    end
+  end
 end
